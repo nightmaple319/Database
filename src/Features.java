85e95a471df099f1cfb5b075ea075e4f1487b16a
Yi-Ming, Chen <nightmaple319@gmail.com>
2017-06-17 00:57:47 +0800

    新增查詢功能1

--- src/Features.java
+++ src/Features.java
@@ -12,10 +12,11 @@ public class Features {
 	static Connection connection;
     static Statement smt;
     static ResultSet rs;
+    static Scanner sc;
     
 	public static void main(String [] args) throws Exception{
 		connectDatabase();
-		Scanner sc = new Scanner(System.in);
+		sc = new Scanner(System.in);
 		int exit = 1;
 		while(exit!=0){
 			System.out.println("請輸入功能代碼： 1.查詢電影   2.查詢演員   3.離開");
@@ -41,11 +42,24 @@ public class Features {
     }
 	
 	private static void searchMovie() throws Exception{
-        ResultSet rs =smt.executeQuery("SELECT * FROM movie");
-        while(rs.next()){
-              String s = "電影名稱：" + rs.getString("movieName") + ",類型：" + rs.getString("category") +",上映時間："+rs.getString("debut");
-              System.out.println(s);
-        }
+		System.out.println("若要列出所有電影請按1，若要由演員來查詢電影請按2，若要由導演來查詢電影請按3");
+		String sCode = sc.nextLine();
+		if(sCode.equals("1")){
+			ResultSet rs =smt.executeQuery("SELECT * FROM movie");
+	        while(rs.next()){
+	              String s = "電影名稱：" + rs.getString("movieName") + ",類型：" + rs.getString("category") +",上映時間："+rs.getString("debut");
+	              System.out.println(s);
+	        }
+		}
+		else if(sCode.equals("2")){
+			System.out.println("請輸入演員名稱");
+			String actName = sc.nextLine();
+			ResultSet rs =smt.executeQuery("SELECT * FROM actor Natural join perform Natural join movie where actName='"+actName+"'");
+	        while(rs.next()){
+	              String s = "電影名稱：" + rs.getString("movieName") + ",類型：" + rs.getString("category") +",上映時間："+rs.getString("debut");
+	              System.out.println(s);
+	        }
+		}
 	}
 	
 	private static void searchActor() throws Exception{
